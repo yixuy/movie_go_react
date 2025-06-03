31ba67fadc3dbddbe332ce2dd9c1ff89590fed3e
henryye <yixuy@sfu.ca>
2025-06-02 23:00:51 -0400

    Finish Json

--- go_movies/go-movies-back-end/cmd/api/handlers.go
+++ go_movies/go-movies-back-end/cmd/api/handlers.go
@@ -1,16 +1,12 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
 	"net/http"
 )
 
 func (app *application) Home(w http.ResponseWriter, r *http.Request) {
-	// fmt.Fprint(w, "Hello, world %s", app.Domain)
-	// var payload = struct {
 
-	// }
 	var payload = struct {
 		Status  string `json:"status"`
 		Message string `json:"message"`
@@ -21,29 +17,16 @@ func (app *application) Home(w http.ResponseWriter, r *http.Request) {
 		Version: "1.0.0",
 	}
 
-	out, err := json.Marshal(payload)
-	if err != nil {
-		fmt.Println(err)
-	}
-
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusOK)
-	w.Write(out)
+	_ = app.writeJSON(w, http.StatusOK, payload)
 }
 
 func (app *application) AllMovies(w http.ResponseWriter, r *http.Request) {
 	movies, err:= app.DB.AllMovies()
 	if err != nil {
-		fmt.Println(err)
+		// fmt.Println(err)
+		app.errorJSON(w, err)
 		return 
 	}
 
-	out, err := json.Marshal(movies)
-	if err != nil {
-		fmt.Println(err)
-	}
-
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusOK)
-	w.Write(out)
+	_ = app.writeJSON(w, http.StatusOK, movies)
 }
